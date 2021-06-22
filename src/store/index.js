69ec20d759eeecbb8174697a227acaf74fed4db6
Ryo SHIINA <ryo.shiina@viacesi.fr>
2021-06-22 18:29:27 +0200

    feat(package.json, requests.js, store/index.js): added axios along with utility functions
    Added axios, with a requests.js file that allows a controlled use of the library. Also edited Vuex
    store to add an user object and a getter object.

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,12 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: null,
+  },
+  getters: {
+    jwt: (state) => (state.user ? state.user.jwt : null),
+  },
   mutations: {},
   actions: {},
   modules: {},
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,9 @@
 <template>
-  <hello-world />
+  <v-container> </v-container>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
 export default {
   name: "Home",
-
-  components: {
-    HelloWorld,
-  },
 };
 </script>
