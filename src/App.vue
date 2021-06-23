4df42b7ee860f9bd8ba1b525159a997ab24a5efa
Ryo SHIINA <ryo.shiina@viacesi.fr>
2021-06-23 03:11:34 +0200

    feat(assts/cesieat.jpg): added CESI'EAT icon and theme colors

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,22 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
+    <v-app-bar app color="primary">
+      <div class="d-flex justify-start">
         <v-img
-          alt="Vuetify Logo"
+          alt="CESI'EAT Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          src="@/assets/cesieat.jpg"
+          height="60"
+          width="60"
         />
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn text color="base">
+        <v-icon class="mr-2">mdi-login</v-icon>
+        <span>Connexion</span>
       </v-btn>
     </v-app-bar>
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,6 +5,34 @@ import fr from "vuetify/lib/locale/fr";
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#FFCD38",
+        secondary: "#4A4A4A",
+        tertiary: "#6C6C6C",
+        base: "#FFFFFF",
+        layer: "#DEDDD7",
+        accent: "#FCDD38",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
+      },
+      dark: {
+        primary: "#FFCD38",
+        secondary: "#4A4A4A",
+        tertiary: "#6C6C6C",
+        base: "#FFFFFF",
+        layer: "#DEDDD7",
+        accent: "#FCDD38",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
+      },
+    },
+  },
   lang: {
     locales: { fr },
     current: "fr",
