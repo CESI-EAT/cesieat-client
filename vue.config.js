cbcbd02b4544cf9e2a839c5d8690580893819a98
Julien Milinkovitch <milinkovitch>
2021-06-28 13:14:50 +0200

    feat: https

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
+import fs from 'fs'
+
 module.exports = {
   transpileDependencies: ['vuetify'],
   devServer: {
+    https: {
+      key: fs.readFileSync('./src/assets/certs/privkey1.pem'),
+      cert: fs.readFileSync('./src/assets/certs/cert1.pem'),
+    },
     disableHostCheck: true,
   },
 }
