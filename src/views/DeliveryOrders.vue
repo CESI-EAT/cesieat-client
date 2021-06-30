00d89cab0840f629a08447ed0b8def64a8e011ce
Antoine PEREZ <antoinealexandre.perez@viacesi.fr>
2021-07-01 01:23:16 +0200

    feat: add orders and history page

---
+++ src/views/DeliveryOrders.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Commandes</h1>
+      </v-col>
+    </v-row>
+    <v-row class="justify-center mt-8" dense>
+      <v-col sm="6" md="8" lg="10" xl="12"
+        ><v-col v-for="(item, i) in items" :key="i" cols="12">
+          <v-card color="grey lighten-4">
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <v-avatar class="ma-3" size="150" tile>
+                <v-img :src="item.src"></v-img>
+              </v-avatar>
+              <v-card-title class="headline">ID Commande</v-card-title>
+              <div>
+                <v-card-title class="headline">Nom du restaurant</v-card-title>
+                <v-card-subtitle>Adresse du restaurant</v-card-subtitle>
+                <v-card-title class="headline">Nom du client</v-card-title>
+                <v-card-subtitle>Adresse du client</v-card-subtitle>
+              </div>
+              <div>
+                <div class="text-center ma-8">
+                  <v-btn color="primary">Accepter</v-btn>
+                </div>
+                <div class="text-center ma-6">
+                  <v-btn width="115" color="error">Refuser</v-btn>
+                </div>
+              </div>
+            </div>
+          </v-card>
+        </v-col>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'DeliveryOrders',
+  data: () => ({
+    items: [
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People',
+      },
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Days',
+        artist: 'Ellie Goulding',
+      },
+    ],
+  }),
+}
+</script>
---
+++ src/views/UserHistory.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Historique des Commandes</h1>
+      </v-col>
+    </v-row>
+    <v-row class="justify-center mt-8" dense>
+      <v-col sm="6" md="8" lg="12" xl="12"
+        ><v-col v-for="(item, i) in items" :key="i" cols="12">
+          <v-card color="grey lighten-4">
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <v-avatar class="ma-3" size="150" tile>
+                <v-img :src="item.src"></v-img>
+              </v-avatar>
+              <v-card-title class="headline">Nom du restaurant</v-card-title>
+              <v-card-title class="headline">Date</v-card-title>
+              <v-card-title class="headline">
+                <ul>
+                  <li>Coffee</li>
+                  <li>Tea</li>
+                  <li>Milk</li>
+                  <li>Coffee</li>
+                </ul></v-card-title
+              >
+              <v-card-title class="headline mr-10">Prix</v-card-title>
+            </div>
+          </v-card>
+        </v-col>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'UserHistory',
+  data: () => ({
+    items: [
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People',
+      },
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Days',
+        artist: 'Ellie Goulding',
+      },
+    ],
+  }),
+}
+</script>
